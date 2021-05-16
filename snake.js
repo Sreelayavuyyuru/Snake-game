c0941f1e4ebefc84a08a989a882eaef3fcf5dafd
Sreelaya Vuyyuru <sreelayavuyyuru@Sreelayas-MacBook-Air.local>
2021-05-16 22:05:01 +0530

    Movement of snake

--- snake.js
+++ snake.js
@@ -1,36 +1,51 @@
 function init() {
     var canvas = document.getElementById('mycanvas');
-    W = canvas.width = 1440;
-    H = canvas.height = 780;
+    W = canvas.width = 1380;
+    H = canvas.height = 720;
     pen=canvas.getContext('2d');
-    cs=62;
+    cs=61;
 
     snake = {
         init_len:5,
-        color: "red",
+        color: "black",
         cells:[],
-        direction: "right",
+        direction:"right",
 
-        createSnake: function(){
+        createSnake:function(){
             for(var i=this.init_len;i>0;i--){
                 this.cells.push({x:i,y:0});
             }
         },
         drawSnake:function(){
             for(var i=0;i<this.cells.length;i++){
-                pen.fillRect(this.cells[i].x*=cs,this.cells[i].y*=cs, cs-2, cs-2);
+                pen.fillStyle = this.color;
+                pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs, cs-1, cs-1);
             }
+        },
+        updateSnake:function(){
+            console.log("updateSnake");
+            this.cells.pop();
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
+            console.log(headX + " " + headY);
+
+            var X=headX+1;
+            var Y=headY;
+            console.log(X+" " + Y);
+
+            this.cells.unshift({x:X, y:Y});
         }
     };
     snake.createSnake();
 }
 
 function draw() {
+    pen.clearRect(0, 0, W, H);
     snake.drawSnake();
 }
 
 function update() {
-
+    snake.updateSnake();
 }
 
 function gameloop() {
